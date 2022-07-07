4e34597d5ba014280182d83b850cae7735fbaacf
wilson.ferreira <willferreirajr@icloud.com>
2022-07-07 07:22:22 -0300

    Cria classe conta e referencias

--- src/main/java/Conta.java
+++ src/main/java/Conta.java
@@ -1,7 +1,30 @@
-class Conta {
+public class Conta {
     double saldo;
     int agencia;
-    int conta;
+    int numero;
     String titular;
+
+    public void deposita(double valor) {
+        this.saldo += valor;
+    }
+
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
 }
+
 
